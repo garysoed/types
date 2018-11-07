9dbd2a777761ee29482cb5b758f36ded97616900
Gary Soedarsono <garysoed@gmail.com>
2018-11-07 00:52:36 -0800

    lint

--- src/element-with-tag-type_test.ts
+++ src/element-with-tag-type_test.ts
@@ -4,26 +4,27 @@ import { ElementWithTagType } from './element-with-tag-type';
 
 describe('check.ElementWithTagType', () => {
   describe('check', () => {
-    should(`should return true if the target is an element with the correct tag name`, () => {
+    should(`return true if the target is an element with the correct tag name`, () => {
       const element = document.createElement('input');
 
       assert(ElementWithTagType('input').check(element)).to.beTrue();
     });
 
-    should(`should return true if the target is an element with the correct tag name with ` +
-        `different case`, () => {
-      const element = document.createElement('input');
+    should(
+        `return true if the target is an element with the correct tag name with different case`,
+        () => {
+          const element = document.createElement('input');
 
-      assert(ElementWithTagType('input').check(element)).to.beTrue();
-    });
+          assert(ElementWithTagType('input').check(element)).to.beTrue();
+        });
 
-    should(`should return false if the target is an element with the wrong tag name`, () => {
+    should(`return false if the target is an element with the wrong tag name`, () => {
       const element = document.createElement('input');
 
       assert(ElementWithTagType('div').check(element)).to.beFalse();
     });
 
-    should(`should return false if the target is not an HTMLElement`, () => {
+    should(`return false if the target is not an HTMLElement`, () => {
       const element = Mocks.object<HTMLDivElement>('element');
 
       assert(ElementWithTagType('div').check(element)).to.beFalse();
