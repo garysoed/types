2189e8742bee6709c378014d778399c853442f05
Gary Soedarsono <garysoed@gmail.com>
2021-05-02 12:58:13 -0700

    Fixed webpack and eslint

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const webpackBuilder = require('dev/webpack/builder');
 const glob = require('glob');
 
 module.exports = webpackBuilder(__dirname)
-    .forDevelopment('[default]', builder => builder
+    .forDevelopment('main', builder => builder
         .addEntry('test', glob.sync('./src/**/*.test.ts'))
         .setOutput('bundle.js', '/out')
         .addTypeScript(),
     )
-    .build();
+    .build('main');
